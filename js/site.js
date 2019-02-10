b0b31f5cbd7d41cae41959d3ca9995437a612c00
Ben <benjamin.schwab.nonspam@gmail.com>
2019-02-09 17:29:04 -0800

    Working, not commented

--- js/site.js
+++ js/site.js
@@ -1,1 +1,28 @@
-console.log("Script Linked!");
+
+window.addEventListener("DOMContentLoaded", scrollLoop, false);
+
+var BigYellowCircle = document.getElementById("BigYellowCircle");
+var GreenPentagon = document.getElementById("GreenPentagon");
+var BlueSquare = document.getElementById("BlueSquare");
+
+
+
+var xScrollPos;
+var yScrollPos;
+
+function scrollLoop(e){
+    
+xScrollPos = window.scrollX;
+yScrollPos = window.scrollY;
+
+setTranslate(xScrollPos * -0.5, yScrollPos * -0.2, BigYellowCircle);
+setTranslate(xScrollPos * -0.4, yScrollPos * -0.4, GreenPentagon);
+setTranslate(xScrollPos * -0.3, yScrollPos * -0.6, BlueSquare);
+
+requestAnimationFrame(scrollLoop);
+}
+
+function setTranslate(xPosition, yPosition, el){
+    el.style.transform = "translate3d(" + xPosition + ", " + yPosition + "px, 0)" ;
+    
+}
